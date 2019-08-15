e805e6f017ab0f748fdb448a5f7e56d671d62c22
Murilo Carvalho <mclibaneo@hotmail.com>
2019-08-14 21:24:27 -0300

    aula 2 completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  photos = [
+    {
+        description: 'menina no trem com fantasma',
+        url: 'https://i.pinimg.com/originals/9a/61/e0/9a61e0715e1ab281c08b9aec48c6d3fe.jpg'
+    },
+    {
+        description: 'menina e totoro com guarda-chuva no pasto',
+        url: 'https://i2.wp.com/geekiss.xpg.com.br/wp-content/uploads/2017/05/geekisspinturas11.jpg?'
+    }    
+];    
 }
